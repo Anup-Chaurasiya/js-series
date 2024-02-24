0875cb45fba4a94ee34c190cbcad9694d3c2a88a
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-02-25 02:30:25 +0530

    datatype

--- 01_basics/01.js
+++ 01_basics/01.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+let constant = undefined;
+
+// console.log(typeof(constant));
+// let number = Number(constant);
+// console.log(typeof constant);
+// console.log(number); 
+
+let constant1 = null;
+
+console.log(typeof constant1)
+let change = String(constant1)
+console.log(typeof change)
+console.log(change)
+
