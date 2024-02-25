4740f6c500219d1d4aaa691e51e4d4b6a06dedea
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-02-25 09:58:59 +0530

    summary on datatype

---
+++ 01_basics/02.js
@@ -0,0 +1,51 @@
+
+let value=3;
+value= -value;
+console.log(value)
+
+// operation 
+
+console.log(2+2);
+console.log(2*2);
+console.log(2/2);
+console.log(2-2);
+console.log(2%3);
+
+let str1= "Anup" ;
+let str2= " Chaurasiya";
+
+// console.log(str1+str2);
+
+console.log("2"==2);
+console.log("2"<2);
+console.log("2"<=2);
+console.log("2"==2);
+console.log(undefined==0);
+
+// // premetive  datatype
+
+// array
+ const constant= ["hello","anup","chaurasiya"];
+
+ console.log(constant);
+
+//  object
+
+let cons1= {
+    id:1234,
+    name:"anup",
+    surname:"chaurasiya"
+};
+
+// function
+
+let integer= function(a){
+    console.log(a);
+}
+
+console.log(typeof constant);
+console.log(typeof cons1);
+console.log(typeof integer);
+
+integer(5,6);
+
