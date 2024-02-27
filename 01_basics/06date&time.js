20013ba93ab0ecd847cbc9a29553ac5e2fa62552
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-02-27 23:41:28 +0530

    date & Time

--- 01_basics/06date&time.js
+++ 01_basics/06date&time.js
@@ -1,12 +1,12 @@
 
 // Date and time
 
-// let date= new Date(2024,10,0);
-// let date1=new Date(2024,11,3);
+let date= new Date(2024,10,0);
+let date1=new Date(2024,11,3);
 
-// console.log(date);
-// console.log(date1.toISOString());
-// console.log(date1.String());
+console.log(date);
+console.log(date1.toISOString());
+console.log(date1.String());
 
 let time= new Date();
 
