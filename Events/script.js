163dc31541f690a21ec2f0bb85a25344327968c4
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-03-08 18:53:04 +0530

    configration done

--- Events/script.js
+++ Events/script.js
@@ -1,79 +1,79 @@
 
-// // there is some meyhod to write apply event listener
-// // onclick
-// // addEventlistener
-// // attachEvent
-// // jQuery-on
+// there is some meyhod to write apply event listener
+// onclick
+// addEventlistener
+// attachEvent
+// jQuery-on
 
-// const delhi=document.querySelector('#delhi')
+const delhi=document.querySelector('#delhi')
 
-// // old approach
+// old approach
 
-// delhi.onclick=function(){
-//     alert('delhi clicked')
-// }
+delhi.onclick=function(){
+    alert('delhi clicked')
+}
 
-// // modern approach
+// modern approach
 
-// // third parameter false don't need to write becoz default case false hota hai
+// third parameter false don't need to write becoz default case false hota hai
 
-// document.querySelector('#photoshop').addEventListener('click',function(e){
-//     console.log(e)
-// },false)
+document.querySelector('#photoshop').addEventListener('click',function(e){
+    console.log(e)
+},false)
 
-// // Event Propagation has 2 types
-// // 1.bubbling
-// // 2.capturing
+// Event Propagation has 2 types
+// 1.bubbling
+// 2.capturing
 
-// // Note ---> bubbling (imp)
+// Note ---> bubbling (imp)
 
-// document.getElementById('image').addEventListener('click',function(e){
-//     console.log("hey Anup how are you ?");
-// },false)
+document.getElementById('image').addEventListener('click',function(e){
+    console.log("hey Anup how are you ?");
+},false)
 
-// document.getElementById('mumbai').addEventListener('click',function(e){
-//     console.log("Goood evening Anup");
-// },false)
-
-
-// // propagation 
+document.getElementById('mumbai').addEventListener('click',function(e){
+    console.log("Goood evening Anup");
+},false)
 
 
-// document.getElementById('image').addEventListener('click',function(e){
-//     console.log("hey Anup how are you ?");
-// },false)
-
-// document.getElementById('mumbai').addEventListener('click',function(e){
-//     console.log("Goood evening Anup");
-//     stopPropagation();
-// },false)
-
-// // capturing
-
-// document.getElementById('image').addEventListener('click',function(e){
-//     console.log("hey Anup how are you ?");
-// },true)
-
-// document.getElementById('mumbai').addEventListener('click',function(e){
-//     console.log("Goood evening Anup");
-// },true)
+// propagation 
 
 
+document.getElementById('image').addEventListener('click',function(e){
+    console.log("hey Anup how are you ?");
+},false)
+
+document.getElementById('mumbai').addEventListener('click',function(e){
+    console.log("Goood evening Anup");
+    stopPropagation();
+},false)
+
+// capturing
+
+document.getElementById('image').addEventListener('click',function(e){
+    console.log("hey Anup how are you ?");
+},true)
+
+document.getElementById('mumbai').addEventListener('click',function(e){
+    console.log("Goood evening Anup");
+},true)
 
 
-// //Prevent default -- Example-->(given below)Google ko open karne se rok sakte hai ye 
 
-// document.getElementById('google').addEventListener('click',function(e){
-//     e.preventDefault()
-//     console.log("Hey rahul how are you");
+
+//Prevent default -- Example-->(given below)Google ko open karne se rok sakte hai ye 
+
+document.getElementById('google').addEventListener('click',function(e){
+    e.preventDefault()
+    console.log("Hey rahul how are you");
     
-// },false)
+},false)
 
 
-// // Task --> we have to remove images one by one 
-// //  when I clicked that Image should be removed
+// Task --> we have to remove images one by one 
+//  when I clicked that Image should be removed
 
-// // apporach
+// apporach
 
 document.querySelector('#image').addEventListener('click',function(e){
     // console.log(e.target.parentNode)
